959974e75a72a1aa9a622b77f756cfc8d629d971
ezratweaver <ezratweaver@gmail.com>
2023-05-01 09:20:01 -0500

    Editing white space

--- utilities.py
+++ utilities.py
@@ -1,6 +1,5 @@
 class Utility():
-    def button_bg_modify(event, color, 
-                    canvas, background_image, 
-                    new_backgroud_image):
+    def button_bg_modify(event, color, canvas,
+                        background_image, new_backgroud_image):
         event.widget.config(bg=color)
         canvas.itemconfigure(background_image, image=new_backgroud_image)
