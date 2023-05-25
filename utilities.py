83da749ca0e99adf87e7a522e13d517ac0ddc6ee
ezratweaver <ezratweaver@gmail.com>
2023-05-25 11:47:29 -0500

    Starting With Clean Slate

--- utilities.py
+++
@@ -1,5 +0,0 @@
-class Utility():
-    def button_bg_modify(event, color, canvas,
-                        background_image, new_backgroud_image):
-        event.widget.config(bg=color)
-        canvas.itemconfigure(background_image, image=new_backgroud_image)
