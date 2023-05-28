cf73bad4f07ded5fbffa4dd6cf351c15be13820c
ezratweaver <ezratweaver@gmail.com>
2023-05-28 15:53:38 -0500

    Added Assets to Code

--- assets.py
+++ assets.py
@@ -14,4 +14,9 @@ class Assets:
     useractionbg = PhotoImage(file="assets/userscreen/useractionbg.png")
     useradd = PhotoImage(file="assets/userscreen/useradd.png")
     userprofile = PhotoImage(file="assets/userscreen/userprofile.png")
-    
+    userlist_1 = PhotoImage(file="assets/userscreen/userlist_1.png")
+    userlist_2 = PhotoImage(file="assets/userscreen/userlist_2.png")
+    userlist_3 = PhotoImage(file="assets/userscreen/userlist_3.png")
+    userlist_4 = PhotoImage(file="assets/userscreen/userlist_4.png")
+    userlist_5 = PhotoImage(file="assets/userscreen/userlist_5.png")
+    userlist_6 = PhotoImage(file="assets/userscreen/userlist_6.png")
