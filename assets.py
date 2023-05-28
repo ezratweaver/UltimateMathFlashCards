64f303de9294fa41a15e9bda103f39e42c9d6c33
ezratweaver <ezratweaver@gmail.com>
2023-05-27 22:12:18 -0500

    Userscreen Asset Code

--- assets.py
+++ assets.py
@@ -1,10 +1,17 @@
 from tkinter import Tk, PhotoImage
+from os import path, chdir
+from sys import argv
+
+EXE_DIR = path.dirname(argv[0])
+chdir(EXE_DIR)
 
 WINDOW_COLOR = "#3556FB"
 
 window = Tk()
 
 class Assets:
-
-    pass
-
+    usertitlebg = PhotoImage(file="assets/userscreen/usertitlebg.png")
+    useractionbg = PhotoImage(file="assets/userscreen/useractionbg.png")
+    useradd = PhotoImage(file="assets/userscreen/useradd.png")
+    userprofile = PhotoImage(file="assets/userscreen/userprofile.png")
+    
