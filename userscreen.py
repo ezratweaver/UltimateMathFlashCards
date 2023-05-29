3703350cdd6f029aead6bc7be2875bdaa229e132
ezratweaver <ezratweaver@gmail.com>
2023-05-29 17:24:16 -0500

    Added Dynamic Background

--- userscreen.py
+++ userscreen.py
@@ -1,6 +1,6 @@
 from tkinter import Canvas
 from assets import window, WINDOW_COLOR
-
+from userdata import get_userlist_banner
 
 class UserScreenGUI:
 
@@ -10,8 +10,13 @@ class UserScreenGUI:
             height=500, width = 800,
             bd=0, highlightthickness=0,
             relief="ridge")
-        
         self.userscreen_canv.pack()
+
+        self.userlist_banner = self.userscreen_canv.create_image(
+            400,
+            250,
+            image=get_userlist_banner()
+        )
 
 userscreen = UserScreenGUI()
 window.geometry("800x500")
