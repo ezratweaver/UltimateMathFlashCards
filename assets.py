7f4ada55ec224a959cf13af1a04274c37d223d67
ezratweaver <ezratweaver@gmail.com>
2023-05-29 17:23:53 -0500

    Got Rid of Class Strucutre

--- assets.py
+++ assets.py
@@ -9,16 +9,17 @@ WINDOW_COLOR = "#3556FB"
 
 window = Tk()
 
-class Assets:
-    userlist_banners = {
-        1: PhotoImage(file="assets/userscreen/userlist_1.png"),
-        2: PhotoImage(file="assets/userscreen/userlist_2.png"),
-        3: PhotoImage(file="assets/userscreen/userlist_3.png"),
-        4: PhotoImage(file="assets/userscreen/userlist_4.png"),
-        5: PhotoImage(file="assets/userscreen/userlist_5.png"),
-        6: PhotoImage(file="assets/userscreen/userlist_6.png"),
-    }
-    usertitlebg = PhotoImage(file="assets/userscreen/usertitlebg.png")
-    useractionbg = PhotoImage(file="assets/userscreen/useractionbg.png")
-    useradd = PhotoImage(file="assets/userscreen/useradd.png")
-    userprofile = PhotoImage(file="assets/userscreen/userprofile.png")
+
+userlist_banners = {
+    1: PhotoImage(file="assets/userscreen/userlist_1.png"),
+    2: PhotoImage(file="assets/userscreen/userlist_2.png"),
+    3: PhotoImage(file="assets/userscreen/userlist_3.png"),
+    4: PhotoImage(file="assets/userscreen/userlist_4.png"),
+    5: PhotoImage(file="assets/userscreen/userlist_5.png"),
+    6: PhotoImage(file="assets/userscreen/userlist_6.png"),
+}
+
+usertitlebg = PhotoImage(file="assets/userscreen/usertitlebg.png")
+useractionbg = PhotoImage(file="assets/userscreen/useractionbg.png")
+useradd = PhotoImage(file="assets/userscreen/useradd.png")
+userprofile = PhotoImage(file="assets/userscreen/userprofile.png")
