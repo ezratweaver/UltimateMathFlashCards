b72700411fe034fb60e5ec585e0aa05e307d7230
ezratweaver <ezratweaver@gmail.com>
2023-05-31 13:12:53 -0500

    Added Dynamic User List

--- userscreen.py
+++ userscreen.py
@@ -1,6 +1,16 @@
 from tkinter import Canvas
+from userdata import get_userlist_banner, get_user_count
 from assets import window, WINDOW_COLOR
-from userdata import get_userlist_banner
+import assets
+
+usertitle_pos = {
+    0 : 250,
+    1 : 218,
+    2 : 186,
+    3 : 154,
+    4 : 121,
+    5 : 88,
+}
 
 class UserScreenGUI:
 
@@ -12,13 +22,25 @@ class UserScreenGUI:
             relief="ridge")
         self.userscreen_canv.pack()
 
+    def print_screen(self) -> None:
         self.userlist_banner = self.userscreen_canv.create_image(
             400,
             250,
             image=get_userlist_banner()
         )
 
+        usercount = get_user_count()
+        start_pos = usertitle_pos.get(usercount)
+        for _ in range(usercount + 1):
+            self.userscreen_canv.create_image(
+                434,
+                start_pos,
+                image=assets.usertitlebg
+            )
+            start_pos = start_pos + 65
+
 userscreen = UserScreenGUI()
+userscreen.print_screen()
 window.geometry("800x500")
 window.title("")
 window.resizable(False, False)
