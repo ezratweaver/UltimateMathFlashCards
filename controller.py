d3ddc545419d48fc342816b80660a1a938078105
ezratweaver <ezratweaver@gmail.com>
2023-06-17 23:32:35 -0500

    Basic Controller Code

---
+++ controller.py
@@ -0,0 +1,12 @@
+from userscreen import UserScreenGUI, root
+
+userscreen = UserScreenGUI()
+
+class Controller:
+    def go_to_mainscreen_check():
+        if userscreen.main_screen:
+            userscreen.main_screen = False
+            userscreen.hide_canvas()
+        root.after(70, Controller.go_to_mainscreen_check)
+
+Controller.go_to_mainscreen_check()
