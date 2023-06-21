d400e0e35ff1aa6310916d5fe00de9771aea8f82
ezratweaver <ezratweaver@gmail.com>
2023-06-21 14:17:49 -0500

    Added hide_viewable_canvas() Function | Screens now in Dictionary

--- controller.py
+++ controller.py
@@ -1,19 +1,25 @@
 from user_screen import UserScreenGUI, root
 import controller_variables
 
-userscreen = UserScreenGUI()
+screens = {"userscreen" : UserScreenGUI(),}
 
 class Controller:
+
+    def hide_viewable_canvas():
+        for _, screen in screens.items():
+            if screen.canvas.winfo_viewable():
+                screen.hide_canvas()
+
     def go_to_mainscreen_check():
         if controller_variables.main_screen:
             controller_variables.main_screen = False
-            userscreen.hide_canvas()
+            Controller.hide_viewable_canvas()
         root.after(70, Controller.go_to_mainscreen_check)
 
     def go_to_profilescreen_check():
         if controller_variables.profile_screen:
             controller_variables.profile_screen = False
-            userscreen.hide_canvas()
+            Controller.hide_viewable_canvas()
         root.after(70, Controller.go_to_profilescreen_check)
 
 Controller.go_to_mainscreen_check()
