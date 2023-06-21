c0a0a5d16abbdfd9dc2882a7a2ed05d6bdbedf31
ezratweaver <ezratweaver@gmail.com>
2023-06-21 14:36:14 -0500

    Main Screen Boilerplate Code

---
+++ main_screen.py
@@ -0,0 +1,12 @@
+from tkinter import Canvas
+from assets import root, WINDOW_COLOR
+import assets
+
+class MainScreenGUI:
+
+    def __init__(self) -> None:
+        self.canvas = Canvas(
+            root, bg=WINDOW_COLOR,
+            height=500, width = 800,
+            bd=0, highlightthickness=0,
+            relief="ridge")
