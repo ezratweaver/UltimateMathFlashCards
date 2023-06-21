5b84cd5d86f7febd7aefc27b8195000301139cd4
ezratweaver <ezratweaver@gmail.com>
2023-06-21 14:51:30 -0500

    Reformatted Button Assets

--- assets.py
+++ assets.py
@@ -14,10 +14,14 @@ root.resizable(False, False)
 
 
 #BUTTON BACKGROUNDS
-buttonbg_long = PhotoImage(
-    file="assets/userscreen/usertitlebg.png")
-buttonbg_square = PhotoImage(
-    file="assets/userscreen/useractionbg.png")
+button_long = PhotoImage(
+    file="assets/buttons/button_long.png")
+button_long_selected = PhotoImage(
+    file="assets/buttons/button_long_selected.png")
+button_square = PhotoImage(
+    file="assets/buttons/button_square.png")
+button_square_selected = PhotoImage(
+    file="assets/buttons/button_square_selected.png")
 #USERSCREEN ASSETS
 userscreen_banners = {
     1: PhotoImage(file="assets/userscreen/userlist_1.png"),
@@ -27,11 +31,8 @@ userscreen_banners = {
     5: PhotoImage(file="assets/userscreen/userlist_5.png"),
     6: PhotoImage(file="assets/userscreen/userlist_6.png"),
 }
-userscreen_usertitlebg_selected = PhotoImage(
-    file="assets/userscreen/usertitlebg_selected.png")
-userscreen_useractionbg_selected = PhotoImage(
-    file="assets/userscreen/useractionbg_selected.png")
 userscreen_useradd = PhotoImage(
     file="assets/userscreen/useradd.png")
 userscreen_userprofile = PhotoImage(
     file="assets/userscreen/userprofile.png")
+
