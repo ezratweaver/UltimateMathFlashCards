1ab91e150c2cab917630bae38bf68047d965376d
ezratweaver <ezratweaver@gmail.com>
2023-06-20 22:48:36 -0500

    Controller Now Checks controller_variables For Changes

--- controller.py
+++ controller.py
@@ -1,17 +1,18 @@
 from user_screen import UserScreenGUI, root
+import controller_variables
 
 userscreen = UserScreenGUI()
 
 class Controller:
     def go_to_mainscreen_check():
-        if userscreen.main_screen:
-            userscreen.main_screen = False
+        if controller_variables.main_screen:
+            controller_variables.main_screen = False
             userscreen.hide_canvas()
         root.after(70, Controller.go_to_mainscreen_check)
 
     def go_to_profilescreen_check():
-        if userscreen.profile_screen:
-            userscreen.profile_screen = False
+        if controller_variables.profile_screen:
+            controller_variables.profile_screen = False
             userscreen.hide_canvas()
         root.after(70, Controller.go_to_profilescreen_check)
 
