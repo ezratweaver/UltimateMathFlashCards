a91f307168ea36bcb40d706bf20cc6db28d89699
ezratweaver <ezratweaver@gmail.com>
2023-06-21 14:44:22 -0500

    Reformatting Assets

--- assets.py
+++ assets.py
@@ -12,7 +12,14 @@ root.geometry("800x500")
 root.title("")
 root.resizable(False, False)
 
-userlist_banners = {
+
+#BUTTON BACKGROUNDS
+buttonbg_long = PhotoImage(
+    file="assets/userscreen/usertitlebg.png")
+buttonbg_square = PhotoImage(
+    file="assets/userscreen/useractionbg.png")
+#USERSCREEN ASSETS
+userscreen_banners = {
     1: PhotoImage(file="assets/userscreen/userlist_1.png"),
     2: PhotoImage(file="assets/userscreen/userlist_2.png"),
     3: PhotoImage(file="assets/userscreen/userlist_3.png"),
@@ -20,15 +27,11 @@ userlist_banners = {
     5: PhotoImage(file="assets/userscreen/userlist_5.png"),
     6: PhotoImage(file="assets/userscreen/userlist_6.png"),
 }
-image_usertitlebg = PhotoImage(
-    file="assets/userscreen/usertitlebg.png")
-image_useractionbg = PhotoImage(
-    file="assets/userscreen/useractionbg.png")
-image_usertitlebg_selected = PhotoImage(
+userscreen_usertitlebg_selected = PhotoImage(
     file="assets/userscreen/usertitlebg_selected.png")
-image_useractionbg_selected = PhotoImage(
+userscreen_useractionbg_selected = PhotoImage(
     file="assets/userscreen/useractionbg_selected.png")
-image_useradd = PhotoImage(
+userscreen_useradd = PhotoImage(
     file="assets/userscreen/useradd.png")
-image_userprofile = PhotoImage(
+userscreen_userprofile = PhotoImage(
     file="assets/userscreen/userprofile.png")
