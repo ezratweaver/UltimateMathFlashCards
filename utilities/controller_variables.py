f677949f829b459f39859a402d221cbc19474051
ezratweaver <ezratweaver@gmail.com>
2024-02-16 22:46:24 -1000

    File structure redesign

--- utilities/controller_variables.py
+++
@@ -1,9 +0,0 @@
-current_user = []
-
-screen_variables = {
-    "main_screen" : False,
-    "profile_screen" : False,
-    "text_screen" : False,
-    "user_screen" : False
-    }
-
--- utilities/encryption.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-
-encryption_key = b'J_z8qk8BrSIhsrjmlKFgVdKBNSjSfop73tkwPxuPc90='
-
-fernet_instance = Fernet(encryption_key)
