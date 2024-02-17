7579839cbde5e4cb46aba78dc0651f70a0439520
ezratweaver <ezratweaver@gmail.com>
2024-02-17 12:45:13 -1000

    Changed main.py interface

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import controller
 # TODO: Link screens with controller
 
 if __name__ == "__main__":
-    controller.screen_objects["user_screen"].show_canvas()
-    controller.user_screen_init()
+    controller.show_userscreen()
     controller.assets.root.mainloop()
