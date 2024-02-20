a399d03af62ced5764419b7e5f254782e3b17273
ezratweaver <ezratweaver@gmail.com>
2024-02-19 19:28:15 -1000

    Removed todo's

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import controller
 
-# TODO: Get memory to work
-# TODO: Link screens with controller
-
 if __name__ == "__main__":
     controller.show_userscreen()
     controller.assets.root.mainloop()
